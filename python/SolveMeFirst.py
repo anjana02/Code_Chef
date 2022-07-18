416d65dfa8c2b733061f3952f14252a7a0bb58e2
anjana_02 <amenu4cse20209@am.students.amrita.edu>
2022-07-18 20:37:36 +0530

    updated

---
+++ python/SolveMeFirst.py
@@ -0,0 +1,12 @@
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    a = num1
+    b = num2
+    return a+b
+    
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
